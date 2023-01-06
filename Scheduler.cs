ed3ae8f765b10f8f3585d18f478a7aa6f59762e6
Felix <50165092+FelixBerinde@users.noreply.github.com>
2023-01-06 03:00:19 -0500

    Add files via upload

---
+++ Scheduler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Appointment_Scheduler_Felix_Berinde.Database;
+
+namespace Appointment_Scheduler_Felix_Berinde
+{
+    public partial class Scheduler : Form
+    {
+        public Scheduler()
+        {
+            InitializeComponent();
+        }
+
+        private void logoffButton_Click(object sender, EventArgs e)
+        {
+            DBConnection.CloseConnection();
+            Application.Exit();
+        }
+    }
+}
