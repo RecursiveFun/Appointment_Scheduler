1346b58d058c1db93106a502137204bca22b15e2
Felix <50165092+FelixBerinde@users.noreply.github.com>
2023-01-27 23:56:26 -0500

    Appointments form work being done
    A work in progress

---
+++ AddAppointment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Appointment_Scheduler_Felix_Berinde
+{
+    public partial class AddAppointment : Form
+    {
+        public AddAppointment()
+        {
+            InitializeComponent();
+        }
+    }
+}
